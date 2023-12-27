93a0527ed5b74458847a74b71f640f27d01f0562
Krutang <pandyakrutang607@gmail.com>
2023-12-27 21:50:36 +0530

    Home Page done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Treading from "./treading/Treading";
+import Popular from './popular/Popular'
+import TopRated from "./topRated/TopRated";
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Treading/>
+      <Popular/>
+      <TopRated/>
     </div>
   );
 };
